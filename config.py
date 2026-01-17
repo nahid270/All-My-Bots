a852012376a16617ebe0f6512b60730423c2c94b
nahid270 <110238003+nahid270@users.noreply.github.com>
2026-01-17 18:23:28 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+REPO_LIST = [
+    # ১ নাম্বার বটের তথ্য
+    {
+        "link": "https://github.com/username/my-chat-bot",  # আপনার বটের গিটহ্যাব লিংক
+        "start_file": "main.py"  # ওই বটের কোন ফাইলটা রান করতে হয় (যেমন: main.py বা bot.py)
+    },
+    
+    # ২ নাম্বার বটের তথ্য
+    {
+        "link": "https://github.com/username/my-music-bot",
+        "start_file": "app.py"
+    },
+    
+    # ৩ নাম্বার বটের তথ্য (এভাবে যত খুশি দিতে পারেন)
+    {
+        "link": "https://github.com/username/filter-bot",
+        "start_file": "bot.py"
+    }
+]
